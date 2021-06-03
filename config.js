dd41db9d15762ee5c751f20f5e418f2d27efb550
Thi1712 <85268656+Thi1712@users.noreply.github.com>
2021-06-03 10:03:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à, mơ đi! :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'Yêu anh vì anh quá đẹp trai lun <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiềuuu 😘😘',
     messDesc: 'Hẹn em ngày 6/6 nhé công chúa của anhhh.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/thi.truong.58511276/' //link mess của các bạn. VD: https://m.me/nam.nodemy
